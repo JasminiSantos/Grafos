d7de505f583b80a29d1d47a9a1f85cb2c987813e
izabelly-code <izabelly.mucholowskiribeiro@gmail.com>
2024-04-19 17:52:49 -0300

    oi

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
     grafo_emails.salvar_lista_adjacencias('grafo_adjacencias_teste.txt')
     grafo_emails.informacoes_grafo()
 
-    # diametro, caminho = grafo_emails.encontrar_diametro()
-    # print("Diâmetro do grafo:", diametro)
-    # print("Caminho do diâmetro:", caminho)
+    diametro, caminho = grafo_emails.encontrar_diametro()
+    print("Diâmetro do grafo:", diametro)
+    print("Caminho do diâmetro:", caminho)
 
-    # resultado = grafo_emails.vertices_ate_distancia_D('anitha.mathis@enron.com', 2)
-    # print("Vértices até a distância 10 de N:", resultado)
+    resultado = grafo_emails.vertices_ate_distancia_D('anitha.mathis@enron.com', 2)
+    print("Vértices até a distância de N:", resultado)
